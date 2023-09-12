6f61ded8b0df692ffe227dba97ecb7e2ac2cac5c
Gilsabo <gilsala_88@hotmail.com>
2023-09-12 18:40:37 +0200

    Fetch images

--- src/App.js
+++ src/App.js
@@ -1,3 +1,51 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 export default function App() {
-  return <div className="App">hello world!</div>;
+  const url = 'https://api.memegen.link/images';
+
+  function getRandomImages(max) {
+    return Math.floor(Math.random() * max);
+  }
+  const [images, setImages] = useState('');
+  const [topText, setTopText] = useState('');
+  const [bottomText, setBottomText] = useState('');
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((res) => {
+        console.log(res.data);
+        setImages(res.data[getRandomImages(198)].url);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const removeDefaultTextImg = () => {
+    const linkWithoutText = [];
+    const words = images.split('/');
+    for (let i = 0; i < 5; i++) {
+      linkWithoutText.push(words[i]);
+    }
+    return linkWithoutText.join('/');
+  };
+
+  console.log(removeDefaultTextImg());
+
+  return (
+    <div className="App">
+      <h1>React meme generator</h1>
+      <img
+        data-test-id="meme-image"
+        src={removeDefaultTextImg()}
+        alt="random"
+      />
+      <div>enter top text {topText}</div>
+      <input onChange={(event) => setTopText(event.currentTarget.value)} />
+      <div>enter bottom text {bottomText}</div>
+      <input onChange={(event) => setBottomText(event.currentTarget.value)} />
+    </div>
+  );
 }
