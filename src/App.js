c5bd8551920574c68f1213ecd7298d71d39ccd0c
Gilsabo <gilsala_88@hotmail.com>
2023-09-12 19:53:43 +0200

    Add fetch post. Need to be fix

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ export default function App() {
     return Math.floor(Math.random() * max);
   }
   const [images, setImages] = useState('');
-  const [topText, setTopText] = useState('');
-  const [bottomText, setBottomText] = useState('');
+  let [topText, setTopText] = useState('dddd');
+  let [bottomText, setBottomText] = useState('');
 
   useEffect(() => {
     axios
@@ -32,16 +32,44 @@ export default function App() {
     return linkWithoutText.join('/');
   };
 
-  console.log(removeDefaultTextImg());
+  useEffect(() => {
+    const urli = 'https://api.memegen.link/images';
+    const templateId = 'aag'; // Replace with your desired template ID
+
+    axios
+      .post(urli, {
+        template_id: templateId,
+        text: [topText, bottomText],
+      })
+      .then((res) => {
+        console.log(res.data);
+        setImages(res.data.url);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  console.log(`${removeDefaultTextImg()}/${encodeURIComponent(topText)}`);
 
   return (
     <div className="App">
       <h1>React meme generator</h1>
-      <img
-        data-test-id="meme-image"
-        src={removeDefaultTextImg()}
-        alt="random"
-      />
+      {!topText && (
+        <img
+          data-test-id="meme-image"
+          src={removeDefaultTextImg()}
+          alt="random"
+        />
+      )}
+      {topText && (
+        <img
+          data-test-id="meme-image"
+          src={`removeDefaultTextImg()/${encodeURIComponent(topText)}`}
+          alt="random"
+        />
+      )}
+
       <div>enter top text {topText}</div>
       <input onChange={(event) => setTopText(event.currentTarget.value)} />
       <div>enter bottom text {bottomText}</div>
